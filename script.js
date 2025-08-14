4211748dc68956aad79c636c5cc29f67180a51fc
simon-fischer-12 <szymon.fischer@gmail.com>
2025-08-14 21:51:53 +0200

    Cleaning
    Cleaner code, convertion to .scss, added web component for footer

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
     thumbnail.classList.add('is-loaded');
   });
 }); 
+
+
+
+class FooterMain extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      
+      <footer id="bottom" class="footer-main">
+        <hr class="footer-divider">
+
+        <div class="footer-text">
+        
+          <h4 class="H4-regular footer-copyright">Copyright 2025</h4>   
+  
+          <div class="contact">
+                <h3 class="H3-regular">szymon.fischer@gmail.com</h3>
+                <h3 class="H3-regular hover-color"><a href="https://www.linkedin.com/in/szymonfischer/" target="_blank">LinkedIn</a></h3>
+                <h3 class="H3-regular hover-color"><a href="https://www.behance.net/s-fischer" target="_blank">Behance</a></h3>
+          </div>
+          
+      </footer>
+    `;
+  }
+}
+
+customElements.define('footer-main', FooterMain);
+
